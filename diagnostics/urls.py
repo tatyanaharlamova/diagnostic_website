3602e4ab3c857956a843b72d6126a719afd413ed
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-09-03 16:00:35 +0300

    Add CBV controllers and forms

--- diagnostics/urls.py
+++ diagnostics/urls.py
@@ -1,11 +1,32 @@
 from django.urls import path
 
 from diagnostics.apps import DiagnosticsConfig
-from diagnostics.views import HomeView, DoctorListView
+from diagnostics.views import HomeView, DoctorListView, DoctorDetailView, DoctorCreateView, DoctorUpdateView, \
+    DoctorDeleteView, ServiceListView, ServiceDetailView, ServiceCreateView, ServiceUpdateView, ServiceDeleteView, \
+    AppointmentListView, AppointmentDetailView, AppointmentCreateView, AppointmentUpdateView, AppointmentDeleteView, \
+    ResultListView, ResultCreateView, ResultUpdateView, ResultDeleteView
 
 app_name = DiagnosticsConfig.name
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
-    path('clients_list/', DoctorListView.as_view(), name='doctors_list')
+    path('doctors_list/', DoctorListView.as_view(), name='doctors_list'),
+    path('doctor/<int:pk>/', DoctorDetailView.as_view(), name='doctor_detail'),
+    path('doctor_create/', DoctorCreateView.as_view(), name='doctor_create'),
+    path('doctor_edit/<int:pk>/', DoctorUpdateView.as_view(), name='doctor_edit'),
+    path('doctor_delete/<int:pk>/', DoctorDeleteView.as_view(), name='doctor_delete'),
+    path('services_list/', ServiceListView.as_view(), name='services_list'),
+    path('service/<int:pk>/', ServiceDetailView.as_view(), name='service_detail'),
+    path('service_create/', ServiceCreateView.as_view(), name='service_create'),
+    path('service_edit/<int:pk>/', ServiceUpdateView.as_view(), name='service_edit'),
+    path('service_delete/<int:pk>/', ServiceDeleteView.as_view(), name='service_delete'),
+    path('appointments_list/', AppointmentListView.as_view(), name='appointments_list'),
+    path('appointment/<int:pk>/', AppointmentDetailView.as_view(), name='appointment_detail'),
+    path('appointment_create/', AppointmentCreateView.as_view(), name='appointment_create'),
+    path('appointment_edit/<int:pk>/', AppointmentUpdateView.as_view(), name='appointment_edit'),
+    path('appointment_delete/<int:pk>/', AppointmentDeleteView.as_view(), name='appointment_delete'),
+    path('results_list/', ResultListView.as_view(), name='results_list'),
+    path('result_create/', ResultCreateView.as_view(), name='result_create'),
+    path('result_edit/<int:pk>/', ResultUpdateView.as_view(), name='result_edit'),
+    path('result_delete/<int:pk>/', ResultDeleteView.as_view(), name='result_delete'),
 ]
